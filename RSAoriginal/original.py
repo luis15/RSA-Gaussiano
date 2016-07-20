e8bade94cf55c2ed1edd2166a61ad15d80e3f403
luis15 <coelho.luisantonio@hotmail.com>
2016-07-19 21:29:50 -0300

    Still not working

--- RSAoriginal/original.py
+++ RSAoriginal/original.py
@@ -1,16 +1,14 @@
 '''
-620031587
-Net-Centric Computing Assignment
-Part A - RSA Encryption
+Luis Antonio Coêlho - 156435 - Universidade Estadual de Campinas - 2016
 '''
 
+import pickle
 import random
 import sys
 
 
 '''
-Euclid's algorithm for determining the greatest common divisor
-Use iteration to make it faster for larger integers
+Algoritmo euclidiano para determinar o MDC
 '''
 def gcd(a, b):
     while b != 0:
@@ -18,7 +16,7 @@ def gcd(a, b):
     return a
 
 '''
-Euclid's extended algorithm for finding the multiplicative inverse of two numbers
+Algoritmo para determinar o inverso multiplicativo de dois números 
 '''
 def multiplicative_inverse(e, phi):
     d = 0
@@ -43,78 +41,115 @@ def multiplicative_inverse(e, phi):
     
     if temp_phi == 1:
         return d + phi
+    else:
+        return phi
 
 '''
-Tests to see if a number is prime.
+Teste de 'primidade'
 '''
 def is_prime(num):
+    num = int(num)
     if num == 2:
         return True
     if (num < 2 or num % 2 == 0):
         return False
-    for n in xrange(3, int(num**0.5)+2, 2):
+    for n in range(3,int(num**0.5)+2, 2):
         if num % n == 0:
             return False
     return True
 
 def generate_keypair(p, q):
     if not (is_prime(p) and is_prime(q)):
-        raise ValueError('Both numbers must be prime.')
+        raise ValueError('Ambos devem ser primos')
     elif p == q:
-        raise ValueError('p and q cannot be equal')
-    #n = pq
+        raise ValueError('p e q não podem ser iguais')
     n = p * q
-
-    #Phi is the totient of n
+    #Phi é totiente de n
     phi = (p-1) * (q-1)
 
-    #Choose an integer e such that e and phi(n) are coprime
+    #Escolhe o inteiro e verifica se ele é coprimo
     e = random.randrange(1, phi)
 
-    #Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    #Usa algoritmo para verificar se phi(n) é coprimo
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
 
-    #Use Extended Euclid's Algorithm to generate the private key
+    #Define a chave
     d = multiplicative_inverse(e, phi)
     
-    #Return public and private keypair
-    #Public key is (e, n) and private key is (d, n)
+    #Chaves públicas são (e, n) e privadas são (d, n)
     return (e, n), (d, n)
 
-def encrypt(pk, plaintext):
-    #Unpack the key into it's components
-    key, n = pk
-    #Convert each letter in the plaintext to numbers based on the character using a^b mod m
-    cipher = [(ord(char) ** key) % n for char in plaintext]
-    #Return the array of bytes
-    return cipher
+def string2numList(strn):
+    """Converts a string to a list of integers based on ASCII values"""
+    return [ ord(chars) for chars in pickle.dumps(strn) ]
 
-def decrypt(pk, ciphertext):
-    #Unpack the key into its components
-    key, n = pk
-    #Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char ** key) % n) for char in ciphertext]
-    #Return the array of bytes as a string
-    return ''.join(plain)
+
+def numList2string(l):
+    """Converts a list of integers to a string based on ASCII values"""
+    return pickle.loads(''.join(map(chr, l)))
+
+
+def numList2blocks(l, n):
+    """Take a list of integers(each between 0 and 127), and combines them
+    into block size n using base 256. If len(L) % n != 0, use some random
+    junk to fill L to make it."""
+    # Note that ASCII printable characters range is 0x20 - 0x7E
+    returnList = []
+    toProcess = copy.copy(l)
+    if len(toProcess) % n != 0:
+        for i in range(0, n - len(toProcess) % n):
+            toProcess.append(random.randint(32, 126))
+    for i in range(0, len(toProcess), n):
+        block = 0
+        for j in range(0, n):
+            block += toProcess[i + j] << (8 * (n - j - 1))
+        returnList.append(block)
+    return returnList
+
+
+def blocks2numList(blocks, n):
+    """inverse function of numList2blocks."""
+    toProcess = copy.copy(blocks)
+    returnList = []
+    for numBlock in toProcess:
+        inner = []
+        for i in range(0, n):
+            inner.append(numBlock % 256)
+            numBlock >>= 8
+        inner.reverse()
+        returnList.extend(inner)
+    return returnList
+
+def encrypt(message, modN, e, blockSize):
+    """given a string message, public keys and blockSize, encrypt using
+    RSA algorithms."""
+    numList = string2numList(message)
+    numBlocks = numList2blocks(numList, blockSize)
+    return [modExp(blocks, e, modN) for blocks in numBlocks]
+
+
+def decrypt(secret, modN, d, blockSize):
+    """reverse function of encrypt"""
+    numBlocks = [modExp(blocks, d, modN) for blocks in secret]
+    numList = blocks2numList(numBlocks, blockSize)
+    return numList2string(numList)
     
 
 if __name__ == '__main__':
-    '''
-    Detect if the script is being run directly by the user
-    '''
-    print ("RSA Encrypter/ Decrypter")
-    p = input("Enter a prime number (17, 19, 23, etc): ")
-    q = input("Enter another prime number (Not one you entered above): ")
-    print ("Generating your public/private keypairs now . . .")
+    print ("RSA Encriptador/Deesncriptador")
+    p = int(input("Insira um primo (17, 19, 23, etc): "))
+    q = int(input("Insira outro primo (Not one you entered above): "))
+    print ("Gerando chaves...")
     public, private = generate_keypair(p, q)
-    print ("Your public key is ", public ," and your private key is ", private)
-    message = input("Enter a message to encrypt with your private key: ")
-    encrypted_msg = encrypt(private, message)
-    print ("Your encrypted message is: ")
+    print ("A chave pública é", public ," e a privada é ", private)
+    message = input("Entre com uma mensagem para encriptá-la com a chave privada: ")
+    encrypted_msg = encrypt(message, public[1], public[0],15)
+    print ("Sua mensagem encriptada é: ")
     print (''.join(map(lambda x: str(x), encrypted_msg)))
-    print ("Decrypting message with public key ", public ," . . .")
-    print ("Your message is:")
-print (decrypt(public, encrypted_msg))
+    print ("Desencriptando a mensagem com a chave pública ", public ," . . .")
+    print ("A mensagem é:")
+    decrypted_msg = decrypt(encrypted_msg, private[1], private[0],15)
+    print (''.join(map(lambda x: str(x), decrypted_msg)))
